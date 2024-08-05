4c671c910fc6d8090063672cfd657216ca453bf5
Peter Teuben <teuben@gmail.com>
2024-08-05 21:04:37 +0000

    proper usage of skip=1

--- mk_runs.py
+++ mk_runs.py
@@ -19,7 +19,7 @@ on["G45.47_03_of_04"] =  [ 119631, 120329, 120330,]
 on["G45.47_04_of_04"] =  [ 120268,]
 
 
-on["G45-mos"] = on["G45.47_01_of_04"]  + on["G45.47_02_of_04"]  + on["G45.47_03_of_04"] + on["G45.47_03_of_04"]
+on["G45-mos"] = on["G45.47_01_of_04"]  + on["G45.47_02_of_04"]  + on["G45.47_03_of_04"] + on["G45.47_04_of_04"]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
@@ -28,7 +28,7 @@ pars1["G45.47_01_of_04"] = "extent=500 b_order=1 dv=60 dw=60 pix_list=-15"
 pars1["G45.47_02_of_04"] = "extent=500 b_order=1 dv=60 dw=60 pix_list=-15"
 pars1["G45.47_03_of_04"] = "extent=500 b_order=1 dv=60 dw=60 pix_list=-15"
 pars1["G45.47_04_of_04"] = "extent=500 b_order=1 dv=60 dw=60 pix_list=-15"
-pars1["G45-mos"]         = "extent=500 b_order=1 dv=60 dw=60 pix_list=-15"
+pars1["G45-mos"]         = "skip=1"
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
@@ -36,14 +36,12 @@ pars2["G45.47_01_of_04"] = "bank=0 pix_list=-13"
 pars2["G45.47_02_of_04"] = "bank=0 pix_list=-13"
 pars2["G45.47_03_of_04"] = "bank=0 pix_list=-13"
 pars2["G45.47_04_of_04"] = "bank=0 pix_list=-13"
-pars2["G45-mos"]         = "bank=0 pix_list=-13"
 
 pars3 = {}
-pars3["G45.47_01_of_04"] = "bank=1"
-pars3["G45.47_02_of_04"] = "bank=1"
-pars3["G45.47_03_of_04"] = "bank=1"
-pars3["G45.47_04_of_04"] = "bank=1"
-pars3["G45-mos"]         = "bank=1"
+pars3["G45.47_01_of_04"] = "bank=1 pix_list=-13,14,15"
+pars3["G45.47_02_of_04"] = "bank=1 pix_list=-13,14,15"
+pars3["G45.47_03_of_04"] = "bank=1 pix_list=-13,14,15"
+pars3["G45.47_04_of_04"] = "bank=1 pix_list=-13,14,15"
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
